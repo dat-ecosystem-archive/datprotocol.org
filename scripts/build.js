36fae5924baa2684a0eac24fc09de2118eef6270
Paul Frazee <pfrazee@gmail.com>
2018-06-20 15:51:57 -0500

    Add active-extensions page (#14)
    * Add active-extensions page
    
    * Dont call extensions active or official
    
    * Add explanation for adding extensions

--- scripts/build.js
+++ scripts/build.js
@@ -10,7 +10,7 @@ var inPath = path.join(__dirname, '..', 'DEPs', 'proposals')
 var template = fs.readFileSync(path.join(__dirname, '..', 'template.html'), 'utf-8')
 var outPath = path.join(__dirname, '..', 'build')
 var depPath = path.join(outPath, 'deps')
-var depList = []
+var extensionsPath = path.join(outPath, 'extensions')
 
 // Clear build dir
 rimraf.sync(outPath)
@@ -20,10 +20,33 @@ mkdirp.sync(depPath)
 fs.writeFileSync(path.join(outPath, 'index.html'), fs.readFileSync(path.join(__dirname, '..', 'site', 'index.html')))
 fs.writeFileSync(path.join(outPath, 'style.css'), fs.readFileSync(path.join(__dirname, '..', 'site', 'style.css')))
 
-fs.readdir(inPath, function (err, list) {
-  if (err) throw err
-  list.filter(function (file) { return file.match(/.*\.md$/) }).map(createPage)
+// Read DEPs directory
+var depFilenames = fs.readdirSync(inPath)
+var depList = depFilenames.filter(function (filename) { return filename.match(/.*\.md$/) }).map(parseDep)
 
+// Read extensions
+var extensions = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'extensions.json'), 'utf8'))
+
+// Generate dynamic pages
+genDEPsIndex(depList)
+genExtensionsIndex(extensions)
+depList.forEach(genDEPPage)
+
+console.log('Done!')
+
+function parseDep (filename) {
+  var inFile = path.join(inPath, filename)
+  var mdContent = fs.readFileSync(inFile, 'utf8')
+
+  var link = `/deps/${filename.replace('.md', '')}`
+  var title = mdContent.split('\n')[1].split('Title:')[1].replace(/\*\*/g, '').trim()
+  var status = mdContent.match(/Status:(.*)\n/)[1].trim().split(' ')[0]
+  var htmlContent = marked(mdContent)
+
+  return {filename, title, link, status, htmlContent}
+}
+
+function genDEPsIndex (depList) {
   var title = 'Dat Enhancement Proposals (DEPs)'
   var mdContent = `
 This page contains a series of "Dat Enhancement Proposal" (DEP) documents, part of the Dat Protocol development and standardization process.
@@ -41,21 +64,34 @@ View [pre-draft DEPs](https://github.com/datprotocol/DEPs/pulls) on GitHub.
 
   var html = template.replace('{source}', marked(mdContent)).replace(/{title}/g, title)
   fs.writeFileSync(path.join(depPath, 'index.html'), html)
-})
+}
 
-function createPage (dep) {
-  var inFile = path.join(inPath, dep)
-  var fileDir = path.join(depPath, dep.replace('.md', ''))
+function genExtensionsIndex (extensions) {
+  var title = 'Dat extensions'
+  var mdContent = `
+Extensions are additional message-types used in the Dat protocol's exchange between computers. They are used to add optional or experimental features to Dat.
+
+Each extension is identified by a token, such as "session-data" or "ping". Developers are free to create and use their own extensions, but should avoid conflicting with any existing tokens. Refer to this list to see which tokens are in use. New extensions can be [proposed as DEPs](https://github.com/datprotocol/DEPs#the-process).
+
+This list includes the extensions which have been formally reviewed and accepted by the Dat Working Group.
+
+### Extensions:
+
+${extensions.map(function (item) {
+  return `* **\`${item.id}\`** [${item.dep}](/deps/${item.dep}/)`
+}).join('\n')}
+
+  `.trim()
+
+  var html = template.replace('{source}', marked(mdContent)).replace(/{title}/g, title)
+  fs.mkdirSync(extensionsPath)
+  fs.writeFileSync(path.join(extensionsPath, 'index.html'), html)
+}
+
+function genDEPPage (dep) {
+  var fileDir = path.join(depPath, dep.filename.replace('.md', ''))
   var outFile = path.join(fileDir, 'index.html')
-  var mdContent = fs.readFileSync(inFile, 'utf8')
-
-  var link = `/deps/${dep.replace('.md', '')}`
-  var title = mdContent.split('\n')[1].split('Title:')[1].replace(/\*\*/g, '').trim()
-  var status = mdContent.match(/Status:(.*)\n/)[1].trim().split(' ')[0]
-  var htmlContent = marked(mdContent)
-
-  depList.push({title, link, status})
-  var html = template.replace('{source}', htmlContent).replace(/{title}/g, title)
+  var html = template.replace('{source}', dep.htmlContent).replace(/{title}/g, dep.title)
   fs.mkdirSync(fileDir)
   fs.writeFileSync(outFile, html)
 }
